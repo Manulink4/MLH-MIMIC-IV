d347b71e29792d9418a2c4f54e3ecfa92b1a2947
Rules99 <100409333@alumnos.uc3m.es>
2021-12-10 11:51:46 +0100

    Updating errors

--- inputfile.py
+++ inputfile.py
@@ -231,131 +231,23 @@ if uploaded_file is not None:
         
     df = getdrugs(option)
     if df is not None:
-
+                
+                #### Filtering
                 df_result = df[df["mol_weight"] < weight_cutoff]
                 df_result2 = df_result[df_result["Logp"] < logp_cutoff]
                 df_result3 = df_result2[df_result2["Donnors"] < NumHDonors_cutoff]
                 df_result4 = df_result3[df_result3["Acceptors"] < NumHAcceptors_cutoff]
-
-                raw_html = mols2grid.display(df_result,  mapping={"smiles": "SMILES","pref_name":"Name","Acceptors":"Acceptors","Donnors":"Donnors","Logp":"Logp","mol_weight":"mol_weight"},
-                subset=["img","Name"],tooltip=["Name","Acceptors","Donnors","Logp","mol_weight"],tooltip_placement="top",tooltip_trigger="click hover")._repr_html_()
-                st.subheader("Compounds for illness {}".format(option))
-                components.html(raw_html, width=900, height=900, scrolling=True)
+                st.subheader("Compounds for {}".format(option))
+                if len(df_result4)==0:
+                    st.write("We have not found some compounds according to these features, try to change the configuration")
+                else:
+                        raw_html = mols2grid.display(df_result,  mapping={"smiles": "SMILES","pref_name":"Name","Acceptors":"Acceptors","Donnors":"Donnors","Logp":"Logp","mol_weight":"mol_weight"},
+                        subset=["img","Name"],tooltip=["Name","Acceptors","Donnors","Logp","mol_weight"],tooltip_placement="top",tooltip_trigger="click hover")._repr_html_()
+            
+                        components.html(raw_html, width=900, height=900, scrolling=True)
     else:
         option = str(option).replace(" ","%20")
         st.markdown(f"""
             We have not found compounds for this illness; for more information visit this link:
-            [Chemble](https://www.ebi.ac.uk/chembl/g/#search_results/all/query={option})
+            [Wikipedia](https://es.wikipedia.org/wiki/{option})
                """, unsafe_allow_html=True)
-# st.write(df_result)
-
-
-
-#st.write(os.listdir())
-#     im = imgGen2(uploaded_file)	
-#     st.image(im, caption='ASCII art', use_column_width=True) 	
-# # if uploaded_file is not None:
-#     src_image = read_image(uploaded_file)
-#     # image = Image.open(uploaded_file)	
-	
-#     st.image(src_image, caption='Input Image', use_column_width=True)
-    #st.write(os.listdir())
-    # im = imgGen2(uploaded_file)	
-    # st.image(im, caption='ASCII art', use_column_width=True) 	
-# model = xrv.models.DenseNet(weights="densenet121-res224-all")
-
-# def asciiart(in_f, SC, GCF,  out_f, color1='black', color2='blue', bgcolor='white'):
-
-#     # The array of ascii symbols from white to black
-#     chars = np.asarray(list(' .,:irs?@9B&#'))
-
-#     # Load the fonts and then get the the height and width of a typical symbol 
-#     # You can use different fonts here
-#     font = ImageFont.load_default()
-#     letter_width = font.getsize("x")[0]
-#     letter_height = font.getsize("x")[1]
-
-#     WCF = letter_height/letter_width
-
-#     #open the input file
-#     img = Image.open(in_f)
-
-
-#     #Based on the desired output image size, calculate how many ascii letters are needed on the width and height
-#     widthByLetter=round(img.size[0]*SC*WCF)
-#     heightByLetter = round(img.size[1]*SC)
-#     S = (widthByLetter, heightByLetter)
-
-#     #Resize the image based on the symbol width and height
-#     img = img.resize(S)
-    
-#     #Get the RGB color values of each sampled pixel point and convert them to graycolor using the average method.
-#     # Refer to https://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/ to know about the algorithm
-#     img = np.sum(np.asarray(img), axis=2)
-    
-#     # Normalize the results, enhance and reduce the brightness contrast. 
-#     # Map grayscale values to bins of symbols
-#     img -= img.min()
-#     img = (1.0 - img/img.max())**GCF*(chars.size-1)
-    
-#     # Generate the ascii art symbols 
-#     lines = ("\n".join( ("".join(r) for r in chars[img.astype(int)]) )).split("\n")
-
-#     # Create gradient color bins
-#     nbins = len(lines)
-#     #colorRange =list(Color(color1).range_to(Color(color2), nbins))
-
-#     #Create an image object, set its width and height
-#     newImg_width= letter_width *widthByLetter
-#     newImg_height = letter_height * heightByLetter
-#     newImg = Image.new("RGBA", (newImg_width, newImg_height), bgcolor)
-#     draw = ImageDraw.Draw(newImg)
-
-#     # Print symbols to image
-#     leftpadding=0
-#     y = 0
-#     lineIdx=0
-#     for line in lines:
-#         color = 'blue'
-#         lineIdx +=1
-
-#         draw.text((leftpadding, y), line, '#0000FF', font=font)
-#         y += letter_height
-
-#     # Save the image file
-
-#     #out_f = out_f.resize((1280,720))
-#     newImg.save(out_f)
-
-
-
-# def imgGen2(img1):
-#   inputf = img1  # Input image file name
-
-#   SC = 0.1    # pixel sampling rate in width
-#   GCF= 2      # contrast adjustment
-
-#   asciiart(inputf, SC, GCF, "results.png")   #default color, black to blue
-#   asciiart(inputf, SC, GCF, "results_pink.png","blue","pink")
-#   img = Image.open(img1)
-#   img2 = Image.open('results.png').resize(img.size)
-#   #img2.save('result.png')
-#   #img3 = Image.open('results_pink.png').resize(img.size)
-#   #img3.save('resultp.png')
-#   return img2	
-
-
-# if uploaded_file is not None:
-#     #src_image = load_image(uploaded_file)
-#     image = Image.open(uploaded_file)	
-	
-#     st.image(uploaded_file, caption='Input Image', use_column_width=True)
-#     #st.write(os.listdir())
-#     im = imgGen2(uploaded_file)	
-#     st.image(im, caption='ASCII art', use_column_width=True) 	
-    
-# name = st.text_input('Name')
-# if not name:
-#             st.warning('Please input a name.')
-#             st.stop()
-#             st.success('Thank you for inputting a name.')
